36750c990c589b7cf753ab201c61d06ac0c2fe3c
jerome <st102710@gsom.spbu.ru>
2022-05-18 18:15:59 +0300

    upload second model

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ MODEL_PATH = "mlmodels/model.pkl"
 SCALER_X_PATH = "mlmodels/scaler_x.pkl"
 SCALER_Y_PATH = "mlmodels/scaler_y.pkl"
 
-MODEL_PATH_final = "mlmodels/model_final.pkl"
-SCALER_X_PATH_final = "mlmodels/scaler_x_final.pkl"
-SCALER_Y_PATH_final = "mlmodels/scaler_y_final.pkl"
+MODEL_PATH_final = "mlmodels/model2.pkl"
+SCALER_X_PATH_final = "mlmodels/scaler_x2.pkl"
+SCALER_Y_PATH_final = "mlmodels/scaler_y2.pkl"
 
 app = Flask(__name__)
 
@@ -39,13 +39,13 @@ def predict():
 
     if model == 2:
 
-        # response = 'floor:{}, open_plan:{}, rooms:{}, area:{}, renovation:{}'.format(floor, open_plan, rooms, area, renovation)
+        # response = 'floor:{}, open_plan:{}, rooms:{}, area:{}.format(floor, open_plan, rooms, area, renovation)
 
         model2 = joblib.load(MODEL_PATH_final)
         sc_x2 = joblib.load(SCALER_X_PATH_final)
         sc_y2 = joblib.load(SCALER_Y_PATH_final)
 
-        x2 = numpy.array([floor, open_plan, rooms, area, renovation]).reshape(1, -1)
+        x2 = numpy.array([floor, open_plan, rooms, area]).reshape(1, -1)
         x2 = sc_x2.transform(x2)
         result2 = model2.predict(x2)
         result2 = sc_y2.inverse_transform(result2.reshape(1, -1))
